5d15cd3dd7748617c79af9ac19057d488c62687a
zivari.ts <zivari.te@gmail.com>
2024-11-16 13:12:27 +0330

    init

---
+++ public/scripts/app.js
@@ -0,0 +1,14 @@
+const toggleThemeBtns = document.querySelectorAll(".toggle-theme");
+
+toggleThemeBtns.forEach(btn=>{
+    btn.addEventListener("click" , () => {
+
+        if (localStorage.theme === "dark"){
+            document.documentElement.classList.remove("dark");
+            localStorage.theme = "light";
+        } else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme" , "dark");
+        }
+    })
+})
